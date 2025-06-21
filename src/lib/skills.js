09541ddb10e65d86154323ee8ea7b93fdb38cc85
David Hurtado <david.h@courthousecomputersystems.com>
2025-06-21 17:08:14 +0200

    feat: add Angular logo and update skills array

--- src/lib/skills.js
+++ src/lib/skills.js
@@ -1,23 +1,23 @@
-import phpLogo from '../assets/images/php.svg';
-import reactLogo from '../assets/images/react.svg';
-import typeScriptLogo from '../assets/images/typescript.svg';
-import nextJsLogo from '../assets/images/nextjs.svg';
-import astroLogo from '../assets/images/astro.svg';
-import wordpressLogo from '../assets/images/wordpress.svg';
-import tailwindLogo from '../assets/images/tailwind.svg';
-import jsLogo from '../assets/images/js.svg';
-import supabaseLogo from '../assets/images/supabase.svg';
+import reactLogo from "../assets/images/react.svg";
+import typeScriptLogo from "../assets/images/typescript.svg";
+import nextJsLogo from "../assets/images/nextjs.svg";
+import astroLogo from "../assets/images/astro.svg";
+import wordpressLogo from "../assets/images/wordpress.svg";
+import tailwindLogo from "../assets/images/tailwind.svg";
+import jsLogo from "../assets/images/js.svg";
+import supabaseLogo from "../assets/images/supabase.svg";
+import angularLogo from "../assets/images/angular.svg";
 
 const skillsArray = [
-  { name: 'React', src: reactLogo },
-  { name: 'Next.js', src: nextJsLogo },
-  { name: 'Astro', src: astroLogo },
-  { name: 'TypeScript', src: typeScriptLogo },
-  { name: 'JavaScript', src: jsLogo },
-  { name: 'PHP', src: phpLogo },
-  { name: 'Wordpress', src: wordpressLogo },
-  { name: 'Tailwind CSS', src: tailwindLogo },
-  { name: 'Supabase', src: supabaseLogo },
+  { name: "Angular", src: angularLogo },
+  { name: "React", src: reactLogo },
+  { name: "Next.js", src: nextJsLogo },
+  { name: "Astro", src: astroLogo },
+  { name: "TypeScript", src: typeScriptLogo },
+  { name: "JavaScript", src: jsLogo },
+  { name: "Wordpress", src: wordpressLogo },
+  { name: "Tailwind CSS", src: tailwindLogo },
+  { name: "Supabase", src: supabaseLogo },
 ];
 
 export default skillsArray;
